1972473f50e772d605afc5428104fe986eb94bc9
vi <56130150+vishrutiii@users.noreply.github.com>
2021-11-23 03:41:30 +0530

    Update read_image.py

--- utils/read_image.py
+++ utils/read_image.py
@@ -5,6 +5,8 @@ import cv2
 import PIL
 from PIL import Image
 import numpy as np
+import matplotlib.pyplot as plt
+
 def read_image(image_path, show = True, module : str = "", cvtColor : bool = True):
     # complete the function
     if module == "opencv":
@@ -16,6 +18,26 @@ def read_image(image_path, show = True, module : str = "", cvtColor : bool = Tru
         except : 
             return image
         finally :
+            if show:
+                cv2.imshow("Image",image)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+    else:
+        image = Image.open(image_path)
+        if show:
+            image.show()
+        return np.array(image)
+
+def resize_image(image_path, show = True, module : str = "" ):
+    if module == "opencv":
+        image = cv2.imread(image_path)
+        try:
+            image = cv2.resize(image, (500, 600), interpolation = cv2.INTER_LINEAR) #780,540 random, INTER_NEAREST
+            return image
+        except : 
+            return image
+        finally :
+            #plt.imshow(image)
             if show:
                 cv2.imshow("Image",image)
             cv2.waitKey(0)
