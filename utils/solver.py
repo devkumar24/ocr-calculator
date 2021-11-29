99d7b7a41f3c0652818359fb344750485ad87ded
DEV KUMAR <59363213+devkumar24@users.noreply.github.com>
2021-11-30 01:09:50 +0530

    added env path

--- utils/solver.py
+++ utils/solver.py
@@ -2,23 +2,20 @@ import os
 import sys
 import time
 import math
+
+sys.path.insert(0, "A:\\Pending Projects\\OCR Calculator\\ocr-calculator\\solver")
 from cv2 import TermCriteria_COUNT
 import pandas as pd
 import numpy as np
-
-filepath = "A:\\Pending Projects\\OCR Calculator\\ocr-calculator\\Data\\fun.txt"
-
-df = pd.read_table(filepath, sep=", ")
-input_fun = np.array(df['Input'])
-output_fun = np.array(df['Output'])
+from bodmas import *
 
 
 def output(input_text : str = ""):
     """
     It will take input as a string and return the output of the given input mathematics problem
     """
-    if input_text in input_fun:
-        return output_fun[np.where(input_fun == input_text)]
-    
-    else :
-        raise ValueError("Required Input Function Not Found")
+    cal_object = Calculator(input_text)
+
+    return cal_object.result
+
+# print(output("6+8*9"))
